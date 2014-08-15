214c4c08b38e61f63825f6a16dae644f826187e5
ravitandon <ravifreek63@gmail.com>
2014-08-15 16:29:56 -0400

    first commit

---
+++ BatchInsert.java
@@ -0,0 +1,107 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.DynamicRelationshipType;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.helpers.collection.MapUtil;
+import org.neo4j.unsafe.batchinsert.BatchInserter;
+import org.neo4j.unsafe.batchinsert.BatchInserters;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class BatchInsert{
+
+private static final String DB_PATH = "./databases/roadNet-CA.db";
+private static final String DATA_PATH = "/home/tandon/data/roadNet-CA.txt";
+private static final int NUM_NODES = 2000000;
+private GraphDatabaseService _graphDb;
+private static final int batchSize = 10000;
+private static RelationshipType connection = DynamicRelationshipType.withName("CONNECTED_TO");
+
+public GraphDatabaseService getGraphDB(){
+      return _graphDb;
+}
+
+public void setGraphDB(GraphDatabaseService g){ _graphDb = g; }
+
+public void createNodes(){
+  Node node;
+  Label label = DynamicLabel.label( "Node" );
+  for(int count = 0; count < NUM_NODES; count++){
+    node = _graphDb.createNode();
+    node.setProperty("id", count);
+    node.addLabel(label);
+  }
+}
+
+public void createRelationBetween(Node from, Node to){
+   from.createRelationshipTo(to, connection);
+}
+
+public void createRelationShips(){
+BufferedReader br = null; 
+try {
+    File file = new File(DATA_PATH);
+    br = new BufferedReader(new FileReader(file));
+    Node from, to;
+    String  thisLine = null;
+    while ((thisLine = br.readLine()) != null) {
+      String[] parts = thisLine.split("\t");
+        from = _graphDb.getNodeById(Long.parseLong(parts[0]));
+        to = _graphDb.getNodeById(Long.parseLong(parts[1]));           
+        createRelationBetween(from, to);
+   }
+} catch(IOException e){
+  e.printStackTrace();
+}finally {
+    try {
+        br.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+}
+
+public void batchDb(){
+        // START SNIPPET: batchDb
+GraphDatabaseService batchDb =
+                BatchInserters.batchDatabase( "databases/roadNet-CA.db");
+        setGraphDB(batchDb);
+        createNodes();
+        createRelationShips();
+        batchDb.shutdown();
+    }
+    
+ /*   public void batchDbWithConfig()
+    {
+        // START SNIPPET: configuredBatchDb
+        Map<String, String> config = new HashMap<>();
+        config.put( "neostore.nodestore.db.mapped_memory", "90M" );
+        GraphDatabaseService batchDb =
+                BatchInserters.batchDatabase( "target/batchdb-example-config", fileSystem, config );
+        // Insert data here ... and then shut down:
+        batchDb.shutdown();
+        // END SNIPPET: configuredBatchDb
+    }*/
+
+public BatchInsert(){
+   batchDb();
+}
+
+public static void main(String[] args){
+   BatchInsert batchInsert = new BatchInsert();    
+}
+
+}
---
+++ ShortestPath.java
@@ -0,0 +1,84 @@
+import org.neo4j.graphalgo.GraphAlgoFactory;
+import java.util.Random;
+import java.util.Date;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph;
+import org.neo4j.graphdb.Path;
+import org.neo4j.graphalgo.PathFinder;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.PathExpanders;
+import java.util.Iterator;
+import org.neo4j.helpers.collection.IteratorUtil;
+import org.neo4j.tooling.GlobalGraphOperations;
+
+public class ShortestPath {
+  private static final String DB_PATH = "/home/tandon/Neo/neo4j-community-2.1.3/scripts/databases/roadNet-CA.db";
+  private static final int MAX_PATH=10;
+  private static PathFinder<Path> Path_Finder;  
+  private static int NUM_NODES = 1000000;
+
+  private static Node getNode(GraphDatabaseService graph, int id){
+  Transaction tx = graph.beginTx();
+  Node node = graph.getNodeById(id);
+  tx.success();
+  return node;
+  }
+  
+ public static void loadGraph(GraphDatabaseService graphDb){
+    System.out.println("Loading the database in memory ....");
+    int count = 0;
+    Transaction tx = graphDb.beginTx();
+    Iterable<Node> nodes = GlobalGraphOperations.at(graphDb).getAllNodes();
+    tx.success();
+    for(Node node : nodes){
+       tx = graphDb.beginTx();
+         count += IteratorUtil.count(node.getRelationships());
+       tx.success();
+    }
+    System.out.println("Count = " + count);  
+    System.out.println("Loaded the database in memory .... \n\n\n");
+  }   
+
+  public static void runQueries(GraphDatabaseService graphDb){
+   Node startNode, endNode;
+   System.out.println("Running Queries Now  .....");
+   Random rand = new Random();
+   int numberQueries = 0, startNodeId, endNodeId;
+   long totalTime = 0, nPaths = 0;
+   while (numberQueries < 100000){
+      startNodeId = rand.nextInt(NUM_NODES);
+      endNodeId = rand.nextInt(NUM_NODES);
+      startNode = getNode(graphDb, startNodeId);
+      endNode = getNode(graphDb, endNodeId);
+      long lStartTime = new Date().getTime();
+      Iterable<Path> paths = Path_Finder.findAllPaths(startNode, endNode);
+      long lEndTime = new Date().getTime();
+      long difference = lEndTime - lStartTime;
+      totalTime += difference;
+      //System.out.println("Query completed..... Time taken = " + difference + " milliseconds. StartNode, EndNode" + startNodeId +"," + endNodeId);
+      nPaths += IteratorUtil.count(paths);
+     
+      //System.out.println("Number of paths :" + nPaths);
+      if (numberQueries % 100 == 0)
+      System.out.println("Query Rate = " + (double)numberQueries/totalTime);
+      numberQueries++;
+   }
+      System.out.println("TotalTime = " + totalTime);
+      System.out.println("TotalPaths = " + nPaths);
+  }
+
+  public static void main(String[] args){
+        GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
+        long lStartTime = new Date().getTime();
+        loadGraph(graphDb);
+        long lEndTime = new Date().getTime();
+        long difference = lEndTime - lStartTime;
+        System.out.println("Time taken to load the database = " + difference/1000 + " milliseconds.");
+        Path_Finder = GraphAlgoFactory.allPaths(PathExpanders.allTypesAndDirections(), MAX_PATH);      
+        runQueries(graphDb);
+        graphDb.shutdown();
+        System.out.println("ShutDown DB Done");
+  }
+} 
